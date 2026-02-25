44f051c7a491923151c76129916be5a466aef960
AppPerfect <appperfect@AppPerfects-Mac-mini.local>
2026-02-25 16:15:05 +0530

    first commit of terraformn

---
+++ provider.tf
@@ -0,0 +1,32 @@
+terraform {
+
+    cloud { 
+    
+    organization = "self_k8s_portal" 
+
+    workspaces { 
+      name = "k8s-dev" 
+    } 
+  } 
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "kubernetes" { 
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
